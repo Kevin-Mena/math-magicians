fd8cae740c1140aec4c8a2ff156820cff9a9a884
Kevin Okoth <kevinokotth04@gmail.com>
2023-03-29 08:42:34 +0300

    Add UI styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import Starter from './components/calculator';
-import Quotes from './components/math-quotes';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Calculator from "./components/calculator";
+import Quotes from "./components/math-quotes";
+import Home from "./components/Home";
 
 const App = () => (
-  <div className="container">
-    <Starter />
-    <Quotes />
-  </div>
+  <BrowserRouter>
+    <nav className="navbar">
+      <h1>Math Magicians</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/calculator">Calculator</Link>
+        </li>
+        <li>
+          <Link to="/quotes">Quote</Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="calculator" element={<Calculator />} />
+      <Route path="quotes" element={<Quotes />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
